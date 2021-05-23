58489c4705752bcdff7a56d9a6419b37f444b55d
Sujanth <sujanth089@googlemail.com>
2021-05-23 12:38:04 +0200

    Revert "payton: Use May fingerprint from Redfin"
    This reverts commit 4d6150f2e382a11155fe8d46ca1929dd413b50d2.

--- lineage_payton.mk
+++ lineage_payton.mk
@@ -74,4 +74,4 @@ PRODUCT_RELEASE_NAME := payton
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=payton
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+ BUILD_FINGERPRINT := "google/coral/coral:11/RQ2A.210505.002/7246365:user/release-keys"
